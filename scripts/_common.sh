f2bc4d862f69d9199f8ed44ce9b3c2dcd2875a7d
Félix Piédallu <felix@piedallu.me>
2024-01-18 14:31:54 +0100

    fix python calls

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -33,11 +33,13 @@ log_file="${log_path}/${app}.log"
 _install_scovie_venv() {
     ynh_exec_as "$app" python3 -m venv --upgrade "$install_dir/venv"
 
+    venvpython="$install_dir/venv/bin/python3"
+
     ynh_add_config --template="requirements.txt" --destination="$install_dir/requirements.txt"
 
-    ynh_exec_as "$app" "$install_dir/venv/bin/python3" -m ensurepip
-    ynh_exec_as "$app" "$install_dir/venv/bin/pip3" install --upgrade wheel pip setuptools
-    ynh_exec_as "$app" "$install_dir/venv/bin/pip3" install --no-deps -r "$install_dir/requirements.txt"
+    ynh_exec_as "$app" "$venvpython" -m ensurepip
+    ynh_exec_as "$app" "$venvpython" -m pip install --upgrade wheel pip setuptools
+    ynh_exec_as "$app" "$venvpython" -m pip install --no-deps -r "$install_dir/requirements.txt"
 }
 
 #=================================================
