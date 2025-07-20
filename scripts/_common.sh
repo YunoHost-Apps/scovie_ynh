62c1b7d659b6f31c02fc944d97778bffbbff5a6a
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-07-20 09:54:36 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -25,10 +25,11 @@ log_path=/var/log/$app
 log_file="${log_path}/${app}.log"
 
 _install_scovie_venv() {
-    ynh_exec_as_app python3 -m venv --upgrade "$install_dir/venv"
-    venvpython="$install_dir/venv/bin/python3"
-    ynh_config_add --template="requirements.txt" --destination="$install_dir/requirements.txt"
-    ynh_exec_as_app "$venvpython" -m ensurepip
-    ynh_exec_as_app "$venvpython" -m pip install --upgrade wheel pip setuptools
-    ynh_exec_as_app "$venvpython" -m pip install --no-deps -r "$install_dir/requirements.txt"
+    pushd $install_dir
+        python3 -m venv $install_dir/venv
+        ynh_config_add --template="requirements.txt" --destination="$install_dir/requirements.txt"
+        ynh_hide_warnings $install_dir/venv/bin/pip ensurepip
+        ynh_hide_warnings $install_dir/venv/bin/pip pip install --upgrade wheel pip setuptools
+        ynh_hide_warnings $install_dir/venv/bin/pip pip install --no-deps -r "$install_dir/requirements.txt"
+    popd
 }
