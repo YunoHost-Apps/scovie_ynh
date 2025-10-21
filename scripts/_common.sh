4a7fcfa206053934808f3f9546617ef9b9a3087e
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-10-21 10:06:39 +0200

    fix permissions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -24,10 +24,10 @@ log_path=/var/log/$app
 log_file="${log_path}/${app}.log"
 
 _install_scovie_venv() {
-    ynh_exec_as_app python3 -m venv --upgrade "$install_dir/venv"
+    ynh_hide_warnings ynh_exec_as_app python3 -m venv --upgrade "$install_dir/venv"
     venvpython="$install_dir/venv/bin/python3"
-    ynh_config_add --template="requirements.txt" --destination="$install_dir/requirements.txt"
-    ynh_exec_as_app "$venvpython" -m ensurepip
-    ynh_exec_as_app "$venvpython" -m pip install --upgrade wheel pip setuptools
-    ynh_exec_as_app "$venvpython" -m pip install --no-deps -r "$install_dir/requirements.txt"
+    ynh_hide_warnings ynh_config_add --template="requirements.txt" --destination="$install_dir/requirements.txt"
+    ynh_hide_warnings ynh_exec_as_app "$venvpython" -m ensurepip
+    ynh_hide_warnings ynh_exec_as_app "$venvpython" -m pip install --upgrade wheel pip setuptools
+    ynh_hide_warnings ynh_exec_as_app "$venvpython" -m pip install --no-deps -r "$install_dir/requirements.txt"
 }
